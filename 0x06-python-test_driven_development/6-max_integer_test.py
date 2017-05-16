3c3a2a53fc862c30b0a22fe044e33b4a08d8da7f
KRLoyd <loydkristen@gmail.com>
2017-05-16 19:59:09 +0000

    Rename 6-max_integer_test.txt to 6-max_integer_test.py Add initial tests for task.

---
+++ 0x06-python-test_driven_development/6-max_integer_test.py
@@ -0,0 +1,11 @@
+# Test list with only ints
+max_integer([1, 2, 3, 4])
+
+# Test list with not an int
+max_integer([a, 1, 2, 4])
+
+# Test no input
+max_integer()
+
+# Tesxt empty list
+max_integer([])
